c65938db5d687ac70cfed2873071e692ca8100c8
Mickey <au587515>
2020-01-09 22:36:52 +0100

    TileWidget, Triangle painters.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,113 @@
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
-
-// import 'package:lyric_notes/layouts/home_desktop.dart';
-import 'package:lyric_notes/layouts/home_mobile.dart';
+import 'package:flutter/services.dart';
+import 'dart:math' as math;
 
 void main() {
+  // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  runApp(MyApp());
+
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Welcome to q-tile',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to q-tile'),
+          ),
+          body: Center(
+            child: TileWidget(),
+          )),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter test app',
-      theme: ThemeData.light(),
-      home: OrientationBuilder(
-        builder: (context, orientation) => buildHome(context),
-      ),
     );
   }
+}
 
-  Widget buildHome(BuildContext context) {
-    // var isLargeScreen = MediaQuery.of(context).size.width > 920;
-    // return isLargeScreen ? HomeLayoutDesktop() : HomeLayoutMobile();
-    return HomeLayoutMobile();
+class TileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return CustomPaint(
+        painter: TilePainter(),
+        child: SizedBox(
+          width: 100,
+          height: 100,
+        ));
+  }
+}
+
+class TilePainter extends CustomPainter {
+  static final color1 = Colors.lightBlue[100];
+  static final color2 = Colors.blue[900];
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.scale(size.width, size.height);
+    var rect = Offset.zero & Size(1, 1);
+    canvas.drawRect(rect, Paint()..color = color1);
+    _paintOpen(canvas);
+    canvas.translate(1, 0);
+    canvas.rotate(math.pi / 2);
+    _paintWhite(canvas);
+    canvas.translate(1, 0);
+    canvas.rotate(math.pi / 2);
+    _paintClosed(canvas);
+    canvas.translate(1, 0);
+    canvas.rotate(math.pi / 2);
+    _paintBlack(canvas);
+
+    // TODO: implement paint
+  }
+
+  void _paintOpen(Canvas canvas) {
+    canvas.save();
+    var linesize = Size(1 / 17, 1);
+    _clipTriangle(canvas);
+    for (var i = 0; i < 9; i++) {
+      canvas.drawRect(
+          Offset(1 / 17 * i * 2, 0) & linesize, Paint()..color = color2);
+    }
+    canvas.restore();
+  }
+
+  void _paintClosed(Canvas canvas) {
+    canvas.save();
+    var linesize = Size(1, 1 / 17);
+    _clipTriangle(canvas);
+    for (var i = 0; i < 9; i++) {
+      canvas.drawRect(
+          Offset(0, 1 / 17 * i * 2) & linesize, Paint()..color = color2);
+    }
+    canvas.restore();
+  }
+
+  void _paintBlack(Canvas canvas) {
+    canvas.save();
+    _clipTriangle(canvas);
+    canvas.drawRect(Offset.zero & Size(1, 1), Paint()..color = color2);
+    canvas.restore(); 
+  }
+
+  void _paintWhite(Canvas canvas) {
+
+  }
+
+  void _clipTriangle(Canvas canvas) {
+    var clipPath = Path();
+    clipPath.moveTo(0, 0);
+    clipPath.lineTo(0.5, 0.5);
+    clipPath.lineTo(1, 0);
+    clipPath.close();
+    canvas.clipPath(clipPath);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return false;
   }
 }
--- lib/models/document.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Document {
-  String title;
-  String body;
-
-  Document({@required this.title, this.body = ""});
-}
